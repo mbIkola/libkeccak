017d897c3ae8ac6d5f4f609be7d170d5cf7392b8
Nickolay Kharchevin <kharchevyn@gmail.com>
2017-10-31 22:46:46 +0200

    Repo submodule forked from crypronight webassembly project. Still Boilerplate

---
+++ test/gtest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by mbikola on 10/31/17.
+//
+
+#include "gtest/gtest.h"
+
+int main(int args, char **argv) {
+    ::testing::InitGoogleTest(&args, argv);
+    return RUN_ALL_TESTS();
+}
